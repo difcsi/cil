82eff57bd2917d640697676a2c7ca5bf42d96fd6
Stephen Kell <stephen.kell@cl.cam.ac.uk>
2014-09-07 14:31:30 +0100

    Naive support for gcc's undocumented "unwind_word" variable mode (see https://gcc.gnu.org/ml/gcc-patches/2008-06/msg00969.html).

--- src/frontc/cabs2cil.ml
+++ src/frontc/cabs2cil.ml
@@ -1760,6 +1760,7 @@ let cabsTypeAddAttributes a0 t =
 			    "byte" -> 1
 			  | "word" -> !Machdep.theMachine.Machdep.sizeof_int
 			  | "pointer" -> !Machdep.theMachine.Machdep.sizeof_ptr
+			  | "unwind_word" -> !Machdep.theMachine.Machdep.sizeof_ptr (* FIXME: always ptrsized? *)
 			  | "QI" -> 1
 			  | "HI" -> 2
 			  | "SI" -> 4
