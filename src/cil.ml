91db50195965d6f188421b7486022ef2de5c4d52
Stephen Kell <stephen.kell@cl.cam.ac.uk>
2018-03-09 12:40:17 +0000

    Add __builtin_memmove() gcc builtin.

--- src/cil.ml
+++ src/cil.ml
@@ -3064,6 +3064,7 @@ let initGccBuiltins () : unit =
   H.add h "__builtin_isnan" (boolType, [], true);
   H.add h "__builtin_memcpy" (voidPtrType, [ voidPtrType; voidConstPtrType; sizeType ], false);
   H.add h "__builtin_memchr" (voidPtrType, [voidConstPtrType; intType; ulongType], false);
+  H.add h "__builtin_memmove" (voidPtrType, [ voidPtrType; voidConstPtrType; sizeType ], false);
   H.add h "__builtin_mempcpy" (voidPtrType, [ voidPtrType; voidConstPtrType; sizeType ], false);
   H.add h "__builtin_memset" (voidPtrType,
                               [ voidPtrType; intType; intType ], false);
