cf551736739f59610bf1a22abb6cd4909b022762
difcsi <zoltan.m@tuta.io>
2026-05-13 18:34:56 +0100

    wip

--- test/testcil.pl
+++ test/testcil.pl
@@ -707,6 +707,12 @@ addTest("testrun/large_unsigned_long");
 addTest("test/shell-escape SHELL_ESCAPE=1");
 addTest("testpipe/simple");
 
+
+addTest("testrun/inline4-gnu89 $gcc OPTIMIZE=1 EXTRAARGS=-O2");
+addTest("testrun/inline4-c11 $gcc OPTIMIZE=1 EXTRAARGS=-O2");
+addTest("testrun/inline4-c11+oldinline $gcc OPTIMIZE=1 EXTRAARGS=-O2");
+
+
 # c99 readiness tests
 addTest("testrunc99/c99-bool");
 addTest("testrunc99/c99-predefined");
